8a5c82cf4e869c59ab431417a5266d5aab48fc24
Shifatu Halimah Olamide <shifatuhalimah20@gmail.com>
2025-05-15 01:22:59 +0100

    Updated code

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const students = [
 const names = students.map((s) => s.name);
 const passed = students.filter((s) => s.score >= 60);
 const total = students.reduce((sum, s) => sum + s.score, 0);
-let removed = students.splice(1, 1);
+let removed = students.splice(1, 1); // Removes Ahmed
 
 function displayResult() {
   let message = "Top Performers:";
@@ -18,9 +18,9 @@ function displayResult() {
 }
 
 document.getElementById("output").innerHTML = `
-    <h2>Names:</h2> ${names.join(", ")} <br><br>
-    <h2>Passed:</h2> ${passed.map((s) => s.name).join(", ")} <br><br>
-    <h2>Total Score:</h2> ${total} <br><br>
-    <h2>Removed Student:</h2> ${removed[0].name} <br><br>
-    <h2>${displayResult()}</h2>
-  `;
+  <h2>Names:</h2> ${names.join(", ")} <br><br>
+  <h2>Passed:</h2> ${passed.map((s) => s.name).join(", ")} <br><br>
+  <h2>Total Score:</h2> ${total} <br><br>
+  <h2>Removed Student:</h2> ${removed[0].name} <br><br>
+  <h2>${displayResult()}</h2>
+`;
